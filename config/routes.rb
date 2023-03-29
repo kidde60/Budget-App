04c01e58d0a4bad36fe8a9168fc20750cefa6b68
kidde60 <kgeorgewilliam60@.com>
2023-03-29 12:55:22 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :transactions do
+  resources :transactions
   resources :categories
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "categories#index"
   # root to: 'home#index'
 
 end
