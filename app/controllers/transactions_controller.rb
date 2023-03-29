c0c19ad8c0afc8e6d6524c5a8b6150385e147dcc
kidde60 <kgeorgewilliam60@.com>
2023-03-29 12:53:59 +0300

    Add category to transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -12,24 +12,22 @@ class TransactionsController < ApplicationController
   # GET /transactions/new
   def new
     @transaction = Transaction.new
+    @categories = Category.where(author: current_user)
+
   end
 
   # GET /transactions/1/edit
   def edit; end
 
   # POST /transactions or /transactions.json
+ 
   def create
-    @transaction = Transaction.new(transaction_params)
+      @transaction = Transaction.new(transaction_params)
+    @transaction.author = current_user
+    return unless @transaction.save
 
-    respond_to do |format|
-      if @transaction.save
-        format.html { redirect_to transaction_url(@transaction), notice: 'Transaction was successfully created.' }
-        format.json { render :show, status: :created, location: @transaction }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
+    CategoryTransaction.create!(transaction_id: @transaction.id, category_id: params[:category])
+    redirect_to category_path(params[:category])
   end
 
   # PATCH/PUT /transactions/1 or /transactions/1.json
@@ -64,6 +62,6 @@ class TransactionsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def transaction_params
-    params.require(:transaction).permit(:name, :amount)
+    params.require(:transaction).permit(:name, :amount, :category)
   end
 end
