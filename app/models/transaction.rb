a5c42ab89ad740b8d6f54cba891aa64bd8c2ba31
kidde60 <kgeorgewilliam60@.com>
2023-04-01 10:55:38 +0300

    Remove unused files

--- app/models/transaction.rb
+++
@@ -1,5 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :category_transactions, dependent: :destroy
-  has_many :categories, through: :category_transactions, dependent: :destroy
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,30 +15,30 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_121305) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.bigint "author_id", null: false
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
-  create_table "category_transactions", force: :cascade do |t|
+  create_table "category_expenses", force: :cascade do |t|
     t.bigint "category_id", null: false
-    t.bigint "transaction_id", null: false
+    t.bigint "expense_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_category_transactions_on_category_id"
-    t.index ["transaction_id"], name: "index_category_transactions_on_transaction_id"
+    t.index ["category_id"], name: "index_category_expenses_on_category_id"
+    t.index ["expense_id"], name: "index_category_expenses_on_expense_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "author_id", null: false
     t.string "name"
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.index ["author_id"], name: "index_transactions_on_author_id"
+    t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_121305) do
   end
 
   add_foreign_key "categories", "users", column: "author_id"
-  add_foreign_key "category_transactions", "categories"
-  add_foreign_key "category_transactions", "transactions"
-  add_foreign_key "transactions", "users", column: "author_id"
+  add_foreign_key "category_expenses", "categories"
+  add_foreign_key "category_expenses", "expenses"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
