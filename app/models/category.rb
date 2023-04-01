6240b3f6744572d2c409e33cc4660a5db9f24869
kidde60 <kgeorgewilliam60@.com>
2023-04-01 10:52:29 +0300

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :category_transactions, dependent: :destroy
-  has_many :transactions, through: :category_transactions, dependent: :destroy
+  has_many :category_expenses, dependent: :destroy
+  has_many :expenses, through: :category_expenses, dependent: :destroy
 end
