dbcf8bd2d6afb5ef4d1c8e3e84cf95bb036e306d
kidde60 <kgeorgewilliam60@.com>
2023-04-01 10:54:37 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :transactions
-  resources :categories
+  
+  resources :categories do
+    resources :expenses
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
